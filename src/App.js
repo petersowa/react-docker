de8a780c9fe713479bfc6596770ffd240ffa22ed
petersowa <petersowa@users.noreply.github.com>
2018-12-04 16:26:15 -0600

    Added support for docker build and testing

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Learn React with Docker 4
           </a>
         </header>
       </div>
